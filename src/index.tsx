3ad77b45be252e3aa3181453e4baf377955cbc5f
IainLR <iainrobertzon@gmail.com>
2021-04-25 18:38:52 -0500

    esbuild setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,41 @@
+import * as esbuild from 'esbuild-wasm'
+import { useState, useEffect } from 'react'
 import ReactDom from 'react-dom'
+import { isTupleTypeNode } from 'typescript'
 
 const App = () => {
-  return <h1>Hello Friend</h1>
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  const startService = async () => {
+    // esbuild initialization, going into public dir for URL
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    })
+    console.log(service)
+  }
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+  const handleClick = () => {
+    console.log(input)
+  }
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
 }
 
 ReactDom.render(<App />, document.querySelector('#root'))
